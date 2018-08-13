99955f4d4fba887edc74224ade04d5123ee46790
Eddú Meléndez <eddu.melendez@gmail.com>
2018-08-12 23:10:39 -0500

    Initial commit

---
+++ src/main/java/com/example/sccpayment/SccPaymentApplication.java
@@ -0,0 +1,12 @@
+package com.example.sccpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccPaymentApplication.class, args);
+	}
+}
